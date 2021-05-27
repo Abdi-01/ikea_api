82b4a1cacdb3378492a2879d77dfb3e70a22d47f
Abdi-01 <abdialghi@gmail.com>
2021-05-27 11:07:08 +0700

    ikea-api : api async_await

--- controllers/productConroller.js
+++ controllers/productConroller.js
@@ -1,67 +1,54 @@
-const { db } = require('../config/database')
+const { db, dbQuery } = require('../config/database')
 
 module.exports = {
-    getProducts: (req, res) => {
-        let dataSearch = [], getSQL, getImage = `Select * from product_image`;
-        let getStock = 'Select * from product_stock ps JOIN status s on ps.idstatus = s.idstatus; '
-        for (let prop in req.query) {
-            dataSearch.push(`${prop} = ${db.escape(req.query[prop])}`)
-        }
-
-        if (dataSearch.length > 0) {
-            getSQL = `Select * from products p JOIN status s on p.idstatus = s.idstatus Where ${dataSearch.join(' AND ')};`
-        } else {
-            getSQL = `Select * from products p JOIN status s on p.idstatus = s.idstatus where p.idstatus=1;`
-        }
-
-        db.query(getSQL, (err, results) => {
-            if (err) {
-                res.status(500).send({ status: 'Error Mysql', messages: err })
+    getProducts: async (req, res) => {
+        try {
+            let dataSearch = [], getSQL, getImage = `Select * from product_image`;
+            let getStock = 'Select * from product_stock ps JOIN status s on ps.idstatus = s.idstatus; '
+            for (let prop in req.query) {
+                dataSearch.push(`${prop} = ${db.escape(req.query[prop])}`)
             }
 
-            db.query(getImage, (err_img, results_img) => {
-                if (err_img) {
-                    res.status(500).send({ status: 'Error Mysql', messages: err_img })
-                }
-                // Looping results data product
-                results.forEach(item => {
-                    // membuat properti images untuk product
-                    item.images = []
-                    // Looping results_img untuk dicocokkan foreign key-nya dgn
-                    // results data products
-                    results_img.forEach(e => {
-                        // jika id sama, data results_img akan dimasukkan kedalam properti baru item.images
-                        if (item.idproduct == e.idproduct) {
-                            item.images.push(e)
-                        }
-                    })
-                });
+            if (dataSearch.length > 0) {
+                getSQL = `Select * from products p JOIN status s on p.idstatus = s.idstatus Where ${dataSearch.join(' AND ')};`
+            } else {
+                getSQL = `Select * from products p JOIN status s on p.idstatus = s.idstatus where p.idstatus=1;`
+            }
 
-                db.query(getStock, (err_stck, results_stck) => {
-                    if (err_stck) {
-                        res.status(500).send({ status: 'Error Mysql', messages: err_stck })
+            let get = await dbQuery(getSQL)
+            let getImg = await dbQuery(getImage)
+            let getStck = await dbQuery(getStock)
+            // Looping results data product
+            get.forEach(item => {
+                // membuat properti images untuk product
+                item.images = []
+                // Looping results_img untuk dicocokkan foreign key-nya dgn
+                // results data products
+                getImg.forEach(e => {
+                    // jika id sama, data results_img akan dimasukkan kedalam properti baru item.images
+                    if (item.idproduct == e.idproduct) {
+                        item.images.push(e)
                     }
-
-                    results.forEach(item => {
-                        item.stock = []
-                        results_stck.forEach(e => {
-                            if (item.idproduct == e.idproduct) {
-                                item.stock.push({
-                                    idproduct_stock: e.idproduct_stock,
-                                    type: e.type,
-                                    qty: e.qty,
-                                    status: e.status
-                                })
-                            }
-                        })
-                    })
-                    console.log(results)
-                    res.status(200).send(results)
                 })
 
-            })
-        })
-
+                item.stock = []
+                getStck.forEach(e => {
+                    if (item.idproduct == e.idproduct) {
+                        item.stock.push({
+                            idproduct_stock: e.idproduct_stock,
+                            type: e.type,
+                            qty: e.qty,
+                            status: e.status
+                        })
+                    }
+                })
+            });
+            res.status(200).send(get)
+        } catch (error) {
+            if (error) {
+                res.status(500).send({ status: 'Error Mysql', messages: error })
+            }
+        }
     },
     addProduct: (req, res) => {
         console.log(req.body)
