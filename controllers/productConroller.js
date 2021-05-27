3f1c9ed7a7847db6209b5fd0fa0492e98b07a55c
Abdi-01 <abdialghi@gmail.com>
2021-05-27 10:26:02 +0700

    ikea-api : update api

--- controllers/productConroller.js
+++ controllers/productConroller.js
@@ -32,7 +32,7 @@ module.exports = {
                     results_img.forEach(e => {
                         // jika id sama, data results_img akan dimasukkan kedalam properti baru item.images
                         if (item.idproduct == e.idproduct) {
-                            item.images.push(e.images)
+                            item.images.push(e)
                         }
                     })
                 });
@@ -77,25 +77,25 @@ module.exports = {
                 res.status(500).send({ status: 'Error Mysql', messages: err })
             }
 
-            console.log("result produk",results)
-            if(results.insertId){
+            console.log("result produk", results)
+            if (results.insertId) {
                 // menjalankan insert untuk product_img dan product_stck
                 let dataImg = []
-                req.body.images.forEach(item=>{
+                req.body.images.forEach(item => {
                     dataImg.push(`(null,${results.insertId},${db.escape(item)})`)
                 })
                 let dataStock = []
-                req.body.stock.forEach(item=>{
+                req.body.stock.forEach(item => {
                     dataStock.push(`(null,${results.insertId},${db.escape(item.type)},${db.escape(item.qty)},${db.escape(req.body.idstatus)})`)
                 })
 
                 // console.log(postImage + dataImg)
                 // console.log(postStock + dataStock)
-                db.query(postImage+dataImg,(err_img,results_img)=>{
+                db.query(postImage + dataImg, (err_img, results_img) => {
                     if (err_img) {
                         res.status(500).send({ status: 'Error Mysql', messages: err_img })
                     }
-                    db.query(postStock+dataStock,(err_stck,results_stck)=>{
+                    db.query(postStock + dataStock, (err_stck, results_stck) => {
                         if (err_stck) {
                             res.status(500).send({ status: 'Error Mysql', messages: err_stck })
                         }
@@ -107,14 +107,50 @@ module.exports = {
         })
 
     },
-    deleteProduct:(req,res)=>{
+    deleteProduct: (req, res) => {
         let delQuery = `Update products set idstatus = 2 where idproduct=${req.query.id};`
-        db.query(delQuery,(err,results)=>{
+        db.query(delQuery, (err, results) => {
             if (err) {
                 res.status(500).send({ status: 'Error Mysql', messages: err })
             }
 
             res.status(200).send("Delete product success ✅")
+        })
+    },
+    updateProduct: (req, res) => {
+        console.log("data update", req.body)
+        let { idproduct, nama, brand, deskripsi, harga, idstatus, images, stock } = req.body
+        let update = `Update products set nama=${db.escape(nama)}, brand=${db.escape(brand)}, deskripsi=${db.escape(deskripsi)},
+        harga=${db.escape(harga)}, idstatus=${db.escape(idstatus)} where idproduct=${db.escape(idproduct)};`
+
+        db.query(update, (err, results) => {
+            if (err) {
+                res.status(500).send({ status: 'Error Mysql', messages: err })
+            }
+            console.log("Update Product Success ✅", results)
+
+            // update images dan stock
+            let updateImages = images.map(item => `Update product_image set images=${db.escape(item.images)} 
+            where idproduct_image=${db.escape(item.idproduct_image)};`)
+            console.log("queryImage", updateImages.join('\n'))
+
+            let updateStocks = stock.map(item => `Update product_stock set type=${db.escape(item.type)},qty=${item.qty} 
+            where idproduct_stock = ${item.idproduct_stock};`)
+
+            db.query(updateImages.join('\n'), (err_img, results_img) => {
+                if (err_img) {
+                    res.status(500).send({ status: 'Error Mysql', messages: err_img })
+                }
+                
+                db.query(updateStocks.join('\n'),(err_stck,results_stck)=>{
+                    if (err_stck) {
+                        res.status(500).send({ status: 'Error Mysql', messages: err_stck })
+                    }
+                    res.status(200).send("Update Product, Stocks and Images Success ✅")
+                })
+
+            })
+
         })
     }
 }
--- routers/productRouter.js
+++ routers/productRouter.js
@@ -4,5 +4,6 @@ const { productController } = require('../controllers')
 router.get('/get-all', productController.getProducts)
 router.post('/post', productController.addProduct)
 router.delete('/delete', productController.deleteProduct)
+router.patch('/update', productController.updateProduct)
 
 module.exports = router
